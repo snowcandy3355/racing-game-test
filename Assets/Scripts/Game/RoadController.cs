5dbda43c25b65bb29bc5068ebf04e3a6b774fbbf
snowcandy3355 <snowcandy3355@gmail.com>
2025-01-24 17:31:25 +0900

    test

---
+++ Assets/Scripts/Game/RoadController.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RoadController : MonoBehaviour
+{
+    [SerializeField] private GameObject[] gasObjects;
+    [SerializeField] private GameObject[] enemyObjects;
+
+
+    private void OnEnable()
+    {
+        // 모든 가스 아이템 비활성
+        foreach (var gasObject in gasObjects)
+        {
+            gasObject.SetActive(false);
+        }
+        // 모든 적 비활성화
+        foreach (var enemyObjects in enemyObjects)
+        {
+            enemyObjects.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// 플레이어 차량이 도로에 진입하면 다음 도로를 생성
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameManager.Instance.SpawnRoad(transform.position + new Vector3(0, 0, 10));
+        }
+    }
+    
+    /// <summary>
+    /// 플레이어 차량이 도로를 벗어나면 해당 도로를 제거
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameManager.Instance.DestroyRoad(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// 랜덤으로 가스 아이템을 표시
+    /// </summary>
+    public void SpawnGas()
+    {
+        int index = Random.Range(0, gasObjects.Length);
+        gasObjects[index].SetActive(true);
+    }
+    /// <summary>
+    /// 랜덤으로 적을 표시
+    /// </summary>
+    public void SpawnEnemy()
+    {
+        int index = Random.Range(0, enemyObjects.Length);
+        enemyObjects[index].SetActive(true);
+    }
+}
